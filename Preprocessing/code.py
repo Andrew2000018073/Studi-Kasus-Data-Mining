31639f10c8cf772e2ad8154fae6238cad941aa64
Andrew2000018073 <77033694+Andrew2000018073@users.noreply.github.com>
2022-12-20 22:19:08 +0700

    Update code.py

--- Preprocessing/code.py
+++ Preprocessing/code.py
@@ -1,1 +1,8 @@
+#Import Library
+import pandas as pd # Library yang sangat dibutuhkan didalam prepocessing. Berguna untuk pembersihan, pengolahan, load data, dan masih banyak lagi
+import numpy as np #Mengolah dan memmproses data dengan kolom atau fitur dengan jumlah yang banyak
+import seaborn as sns # Visualisasi data
+import matplotlib.pyplot as plt #Visualisasi data
+%matplotlib inline
 
+#
